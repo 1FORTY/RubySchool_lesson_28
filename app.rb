90742c2d6acc09d3b2f2c36fb8c9224db3ab144b
Ivan Sklyarenko <wanya.workmail@gmail.com>
2020-08-20 08:55:08 +0700

    Add coments

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require 'sinatra/reloader'
 require 'sqlite3'
 
 def init_db
+  # Инициализируем в переменную БД и добавляем хэши
   @db = SQLite3::Database.new 'leprosorium.db'
   @results_as_hash = true
 
@@ -12,10 +13,12 @@ def init_db
 end
 
 before do
+  # Добавляем БД во все запросы
   init_db
 end
 
 configure do
+  # Создаём таблицу, если не созданан
   db = init_db
   db.execute 'CREATE TABLE IF NOT EXISTS "Posts" (
 	  "id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
